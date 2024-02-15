eb9dbc0fe4a882c2fd3f1e5a36f9ede80be11da1
lasith <lasithamanda1999@gmail.com>
2024-02-15 16:16:57 +0530

    change the code

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Fullstack Developer", "Frontend Developer", "Backend Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
