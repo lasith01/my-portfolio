bd20605ab84b998db560c7d4afbdab718cf08843
lasith <lasithamanda1999@gmail.com>
2024-02-17 00:03:05 +0530

    updated

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer', 'UI/UX Developer'],
+    strings: ['Software Developer', 'Fullstack Developer', 'UI/UX Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
